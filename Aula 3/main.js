73987953340b9bf930aff237b1c4050ba642a3cf
Jessiellen <jessiellen@hotmail.com>
2025-02-07 21:34:10 +0000

    Aula 3

--- Aula 3/main.js
+++ Aula 3/main.js
@@ -1,41 +1,92 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Veiculo = /** @class */ (function () {
-    function Veiculo(marca, modelo, ano) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-    }
-    return Veiculo;
-}());
-var Carro = /** @class */ (function (_super) {
-    __extends(Carro, _super);
-    function Carro(marca, modelo, ano, portas) {
-        var _this = _super.call(this, marca, modelo, ano) || this;
-        _this.portas = portas;
-        return _this;
-    }
-    Carro.prototype.getPortas = function () {
-        return this.portas;
-    };
-    return Carro;
-}(Veiculo));
-var Meucarro = new Carro('Toyota', 'Corolla', 2020, 4);
-console.log(Meucarro.marca);
-console.log(Meucarro.ano);
-console.log(Meucarro.modelo);
-console.log(Meucarro.getPortas());
+// Exercicio 1
+// 
+// class Veiculo {
+//     marca: string;
+//     modelo: string;
+//     ano: number;
+//     constructor(marca: string, modelo: string, ano: number) {
+//         this.marca = marca;
+//         this.modelo = modelo;
+//         this.ano = ano;
+//     }
+//     description(): string {
+//         return `Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}`;
+//     }
+// }
+// class Carro extends Veiculo {
+//     portas: number;
+//     constructor(marca: string, modelo: string, ano: number, portas: number) {
+//         super(marca, modelo, ano);  
+//         this.portas = portas;
+//     }
+//     getPortas(): number {
+//         return this.portas;      
+//     }
+//     description(): string {
+//         return `${super.description()} com ${this.portas} portas.`;
+//     }
+// }
+// const Meucarro = new Carro('Toyota', 'Corolla', 2020, 4);
+// console.log(Meucarro.marca);  
+// console.log(Meucarro.ano);    
+// console.log(Meucarro.modelo); 
+// console.log(Meucarro.getPortas()); 
+// console.log(Meucarro.description());
 // A "getPortas" é para permitir ver a quantidade de portas, codigo ficar mais organizado;
+// Exercicio 2
+// interface Player {
+//     name: string;
+//     age: number;
+//     game: string;
+//   }
+//   const playerPartial: Partial<Player> = {
+//     name: 'Lucas',
+//     game: 'FIFA',
+//   };
+//   console.log(playerPartial);  
+//   const playerReadonly: Readonly<Player> = {
+//     name: 'Mariana',
+//     age: 25,
+//     game: 'Overwatch',
+//   };
+//   console.log(playerReadonly);
+//   type PlayerPreview = Pick<Player, 'name' | 'game'>;
+//   const playerPick: PlayerPreview = {
+//     name: 'Carlos',
+//     game: 'Valorant',
+//   };
+//   console.log(playerPick);
+//   type PlayerInfo = Omit<Player, 'age'>;
+//   const playerOmit: PlayerInfo = {
+//     name: 'Alice',
+//     game: 'League of Legends',
+//   };
+//   console.log(playerOmit);  
+//   const player: Player = {
+//     name: 'Pedro',
+//     age: 30,
+//     game: 'Minecraft',
+//   };
+//   console.log(player);
+var ListHandler = /** @class */ (function () {
+    function ListHandler() {
+        this.items = [];
+    }
+    ListHandler.prototype.add = function (item) {
+        this.items.push(item);
+    };
+    ListHandler.prototype.remove = function (item) {
+        this.items = this.items.filter(function (i) { return i !== item; });
+    };
+    ListHandler.prototype.getAll = function () {
+        return this.items;
+    };
+    return ListHandler;
+}());
+var listHandler = new ListHandler();
+listHandler.add("Jessie");
+listHandler.add("Satoro");
+listHandler.add("Kaneki");
+listHandler.add("Salém");
+listHandler.remove("Salém");
+console.log(listHandler.getAll());
--- Aula 3/main.ts
+++ Aula 3/main.ts
@@ -1,42 +1,123 @@
-class Veiculo {
-    marca: string;
-    modelo: string;
-    ano: number;
+// Exercicio 1 //
+// 
+// class Veiculo {
+//     marca: string;
+//     modelo: string;
+//     ano: number;
 
-    constructor(marca: string, modelo: string, ano: number) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-    }
+//     constructor(marca: string, modelo: string, ano: number) {
+//         this.marca = marca;
+//         this.modelo = modelo;
+//         this.ano = ano;
+//     }
 
-    description(): string {
-        return `Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}`;
-    }
-}
+//     description(): string {
+//         return `Marca: ${this.marca}, Modelo: ${this.modelo}, Ano: ${this.ano}`;
+//     }
+// }
 
-class Carro extends Veiculo {
-    portas: number;
+// class Carro extends Veiculo {
+//     portas: number;
 
-    constructor(marca: string, modelo: string, ano: number, portas: number) {
-        super(marca, modelo, ano);  
-        this.portas = portas;
-    }
+//     constructor(marca: string, modelo: string, ano: number, portas: number) {
+//         super(marca, modelo, ano);  
+//         this.portas = portas;
+//     }
 
-    getPortas(): number {
-        return this.portas;      
-    }
+//     getPortas(): number {
+//         return this.portas;      
+//     }
 
-    description(): string {
-        return `${super.description()} com ${this.portas} portas.`;
-    }
-}
+//     description(): string {
+//         return `${super.description()} com ${this.portas} portas.`;
+//     }
+// }
 
-const Meucarro = new Carro('Toyota', 'Corolla', 2020, 4);
+// const Meucarro = new Carro('Toyota', 'Corolla', 2020, 4);
 
-console.log(Meucarro.marca);  
-console.log(Meucarro.ano);    
-console.log(Meucarro.modelo); 
-console.log(Meucarro.getPortas()); 
-console.log(Meucarro.description());
+// console.log(Meucarro.marca);  
+// console.log(Meucarro.ano);    
+// console.log(Meucarro.modelo); 
+// console.log(Meucarro.getPortas()); 
+// console.log(Meucarro.description());
 
 // A "getPortas" é para permitir ver a quantidade de portas, codigo ficar mais organizado;
+
+// Exercicio 2 //
+
+// interface Player {
+//     name: string;
+//     age: number;
+//     game: string;
+//   }
+  
+//   const playerPartial: Partial<Player> = {
+//     name: 'Lucas',
+//     game: 'FIFA',
+//   };
+  
+//   console.log(playerPartial);  
+  
+//   const playerReadonly: Readonly<Player> = {
+//     name: 'Mariana',
+//     age: 25,
+//     game: 'Overwatch',
+//   };
+  
+//   console.log(playerReadonly);
+
+  
+//   type PlayerPreview = Pick<Player, 'name' | 'game'>;
+  
+//   const playerPick: PlayerPreview = {
+//     name: 'Carlos',
+//     game: 'Valorant',
+//   };
+  
+//   console.log(playerPick);
+  
+  
+//   type PlayerInfo = Omit<Player, 'age'>;
+  
+//   const playerOmit: PlayerInfo = {
+//     name: 'Alice',
+//     game: 'League of Legends',
+//   };
+  
+//   console.log(playerOmit);  
+  
+//   const player: Player = {
+//     name: 'Pedro',
+//     age: 30,
+//     game: 'Minecraft',
+//   };
+  
+//   console.log(player);
+  
+// Exercicio 3 //
+class ListHandler<T> {
+    items: T[] = [];
+
+    add(item: T): void {
+        this.items.push(item);
+    }
+
+    remove(item: T): void {
+        this.items = this.items.filter(i => i !== item);
+    }
+
+    getAll(): T[] {
+        return this.items
+    }
+}
+
+const listHandler = new ListHandler<string>();
+
+listHandler.add("Jessie");
+listHandler.add("Satoro");
+listHandler.add("Kaneki");
+listHandler.add("Salém");
+listHandler.remove("Salém");
+
+
+console.log(listHandler.getAll());
